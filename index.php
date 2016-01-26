e7afe4183d9497121a962c9a2de3b2577b34eb47
Kevin Garibo <garibokevin@gmail.com>
2016-01-25 18:21:34 -0600

    Estado basico para un tema

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+
+<html <?php language_attributes(); ?> >
+<head>
+	<meta content="<?php bloginfo('html_type'); ?>" charset="<?php bloginfo('charset'); ?>">
+	<title> <?php bloginfo('name'); ?> <?php wp_title(); ?></title>
+	<meta name="generator" content="WordPress <?php bloginfo('version'); ?>">
+
+	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url'); ?>" media="screen">
+	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url') ?>">
+	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url') ?>">
+	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url') ?>">
+	<link rel="pingback" href="<?php bloginfo('pingback_url') ?>">
+	<link rel="shortcut icon" type="image/x-icon" href="<?php bloginfo('stylesheet_directory')/favicon.ico ?>">
+</head>
+<body>
+
+	<h1><?php bloginfo('name'); ?></h1>
+
+</body>
+</html>
